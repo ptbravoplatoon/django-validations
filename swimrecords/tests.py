6d0d78438ca93702484825ca523356fc56446193
Joshua Alletto <joshalletto@gmail.com>
2019-01-29 11:07:50 -0600

    add numbers to tests and add gitignore.

--- swimrecords/tests.py
+++ swimrecords/tests.py
@@ -8,7 +8,7 @@ class SwimRecordTestCase(TestCase):
 
   record = SwimRecord()
 
-  def test_validate_first_name_presence(self):
+  def test_01_validate_first_name_presence(self):
     """validates presence of first_name"""
     try: 
       self.record.full_clean()
@@ -16,7 +16,7 @@ class SwimRecordTestCase(TestCase):
       self.assertTrue('This field cannot be blank.' in e.message_dict['first_name'])
 
 
-  def test_validate_last_name_presence(self):  
+  def test_02_validate_last_name_presence(self):  
     """validates presence of last_name"""
     try: 
       self.record.full_clean()
@@ -24,7 +24,7 @@ class SwimRecordTestCase(TestCase):
       self.assertTrue('This field cannot be blank.' in e.message_dict['last_name'])
 
 
-  def test_validate_team_name_presence(self):
+  def test_03_validate_team_name_presence(self):
     """validates presence of team_name"""
     try: 
       self.record.full_clean()
@@ -32,7 +32,7 @@ class SwimRecordTestCase(TestCase):
       self.assertTrue('This field cannot be blank.' in e.message_dict['team_name'])
 
 
-  def test_validate_relay_presence(self):
+  def test_04_validate_relay_presence(self):
     """validates presence of relay"""
     try: 
       self.record.full_clean()
@@ -40,7 +40,7 @@ class SwimRecordTestCase(TestCase):
       self.assertTrue("'None' value must be either True or False." in e.message_dict['relay'])
   
 
-  def test_valid_stroke(self):
+  def test_05_valid_stroke(self):
     """validates that the stroke is one of 'front crawl', 'butterfly', 'breast', 'back', or 'freestyle'"""
     stroke_record = SwimRecord(stroke='doggie paddle')
     try: 
@@ -48,7 +48,7 @@ class SwimRecordTestCase(TestCase):
     except ValidationError as e:
       self.assertTrue("doggie paddle is not a valid stroke" in e.message_dict['stroke'])
   
-  def test_valid_distance(self):
+  def test_06_valid_distance(self):
     """must be greater than or equal to 50"""
     distance_record = SwimRecord(distance=20)
     try: 
@@ -56,7 +56,7 @@ class SwimRecordTestCase(TestCase):
     except ValidationError as e:
       self.assertTrue("20 is not greater than or equal to 50" in e.message_dict['distance'])
 
-  def test_no_future_records(self):
+  def test_07_no_future_records(self):
     """does not allow records to be set in the future"""
     record_broken = datetime.today() + timedelta(days=1)
     record = SwimRecord(record_date=record_date)
@@ -66,7 +66,7 @@ class SwimRecordTestCase(TestCase):
       self.assertTrue("Can't set record in the future." in e.message_dict['record_date'])
 
   
-  def test_no_break_record_before_set_record(self):
+  def test_08_no_break_record_before_set_record(self):
     """does not allow records to be broken before the record_date"""
     record_date = datetime.today()
     record_broken_date = datetime.today() - timedelta(days=1)
