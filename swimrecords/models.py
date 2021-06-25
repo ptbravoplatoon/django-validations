9085d6479eb0652696e6a4ce60689c46a2a6cedb
JessMelvilleGoodin <8205019+JessMelvilleGoodin@users.noreply.github.com>
2021-06-24 23:07:38 -0400

    Completed Validation tests

--- swimrecords/models.py
+++ swimrecords/models.py
@@ -1,12 +1,41 @@
-from django.db import models
+from django.db import models 
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
 
 class SwimRecord(models.Model):
-    pass # delete me when you start writing in validations
-    # first_name = models.CharField()
-    # last_name = models.CharField()
-    # team_name = models.CharField()
-    # relay = models.BooleanField()
-    # stroke = models.CharField()
-    # distance = models.IntegerField()
-    # record_date = models.DateTimeField()
-    # record_broken_date = models.DateTimeField()
+    # id = models.AutoField(primary_key=True)
+    # pass
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    team_name = models.CharField(max_length=50, blank=False)
+    relay = models.BooleanField(null=False)
+
+    STROKE_CHOICES = (
+        ("FRONT_CRAWL",'front crawl'),
+        ("BUTTERFLY", 'butterfly'),
+        ("BREAST", 'breast'),
+        ("BACK", 'back'),
+        ("FREESTYLE",'freestyle')
+    )
+    stroke = models.CharField(max_length=50, choices=STROKE_CHOICES)
+    distance = models.IntegerField(validators=[MinValueValidator(50)])
+
+    def validate_record_date(value):
+        today = timezone.now()
+        if value > today:
+            raise ValidationError("Can't set record in the future.")
+        else:
+            return value
+    record_date = models.DateTimeField(validators=[validate_record_date])
+    
+    def validate_record_broken_date(value):
+        today = timezone.now()
+        if value < today:
+            raise ValidationError("Can't break record before record was set.")
+        else:
+            return value
+    record_broken_date = models.DateTimeField(validators=[validate_record_broken_date])
