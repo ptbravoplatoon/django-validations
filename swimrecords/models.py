7803a960d0858aa46e676d696a60ea0ddd5746d3
adamziolk <adam.lawrence.ziolkowski@gmail.com>
2021-06-24 20:34:04 -0600

    Django Validation

--- swimrecords/models.py
+++ swimrecords/models.py
@@ -1,12 +1,46 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinLengthValidator, MinValueValidator
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 class SwimRecord(models.Model):
-    pass # delete me when you start writing in validations
-    # first_name = models.CharField()
-    # last_name = models.CharField()
-    # team_name = models.CharField()
-    # relay = models.BooleanField()
-    # stroke = models.CharField()
-    # distance = models.IntegerField()
-    # record_date = models.DateTimeField()
-    # record_broken_date = models.DateTimeField()
+    first_name = models.CharField(max_length = 30, validators = [MinLengthValidator(1)])
+    last_name = models.CharField(max_length = 30, validators = [MinLengthValidator(1)])
+    team_name = models.CharField(max_length = 30, validators = [MinLengthValidator(1)])
+    relay = models.BooleanField(blank = False)
+
+    STROKE_CHOICES = [
+        ('front crawl', 'front crawl'), 
+        ('butterfly', 'butterfly'), 
+        ('breast', 'breast'), 
+        ('back', 'back'), 
+        ('freestyle', 'freestyle')
+    ]
+    stroke = models.CharField(max_length = 30, choices = STROKE_CHOICES)
+
+    distance = models.IntegerField(validators = [MinValueValidator(50)])
+
+    def no_future_dates(value):
+        if value > timezone.now():
+            raise ValidationError("Can't set record in the future.")
+        return value
+    record_date = models.DateTimeField(validators = [no_future_dates])
+
+    # def save(self, *args, **kwargs):
+    #     if self.record_broken_date < self.record_date:
+    #         raise ValidationError("Can't break record before record was set.")
+    #     super(SwimRecord, self).save(*args, **kwargs)
+
+
+    # def no_future_record_breaks(self):
+    #     if self.record_broken_date < self.record_date:
+    #         raise ValidationError("Can't break record before record was set.")
+    #     pass
+    # record_broken_date = models.DateTimeField(validators = [no_future_record_breaks])
+
+    # def full_clean(self, *args, **kwargs):
+    #     if self.record_broken_date < self.record_date:
+    #         raise ValidationError("Can't break record before record was set.")
+    #     super(SwimRecord, self).full_clean(*args, **kwargs)
+    record_broken_date = models.DateTimeField()
+
